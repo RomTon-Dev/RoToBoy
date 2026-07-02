5da43fb8c375189a239da00e57c61698f1b1889e
td725 <tom.davies25@imperial.ac.uk>
2026-07-02 16:06:26 +0100

    feat: added header files for cpu, mmu, and cartridge and implimented basic structs and functions.

---
+++ includes/cpu.h
@@ -0,0 +1,59 @@
+#ifndef CPU_H
+#define CPU_H
+
+#include "mmu.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+    // 8-bit and 16-bit General Purpose Registers paired using unions.
+    union {
+        struct {
+            uint8_t f;
+            uint8_t a;
+        }; // Note: Game Boy is Little-Endian,
+        uint16_t af; // so lower byte (F) comes first in memory.
+    };
+
+    union {
+        struct {
+            uint8_t c;
+            uint8_t b;
+        };
+        uint16_t bc;
+    };
+
+    union {
+        struct {
+            uint8_t e;
+            uint8_t d;
+        };
+        uint16_t de;
+    };
+
+    union {
+        struct {
+            uint8_t l;
+            uint8_t h;
+        };
+        uint16_t hl;
+    };
+
+    // Control Registers
+    uint16_t sp; // Stack Pointer
+    uint16_t pc; // Program Counter
+
+    // Internal CPU State
+    bool master_interrupt_enable; // IME flag (controlled by EI and DI instructions)
+    bool halted; // CPU enters low-power halt state
+    bool stopped; // CPU enters very low-power stop state
+
+    // Pointer to the system Bus/MMU so the CPU can read/write memory
+    Bus* bus;
+} CPU;
+
+// Core API Functions
+void cpu_init(CPU* cpu, Bus* bus); // Initialise CPU with default state
+uint8_t cpu_step(CPU* cpu); // Executes 1 instruction, returns M-cycles taken
+
+#endif
