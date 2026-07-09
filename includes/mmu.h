84fcd861a6d5a068a5247844d03b7b591b7a2a0a
td725 <tom.davies25@imperial.ac.uk>
2026-07-09 20:02:37 +0100

    chore: changed mmu.h to have more  helpful comment, be more readable and contain the releavant IO registers.

--- includes/mmu.h
+++ includes/mmu.h
@@ -6,18 +6,28 @@
 #include <stdint.h>
 
 typedef struct {
-    Cartridge cart; // External rom cartridge
+    // EXTERNAL SUB-COMPONENTS (they manage their own IO)
 
-    // Memory located on chip (not in cartridge). Possibly move VRAM to PPU struct once implimented
-    uint8_t vram[0x2000]; // 8 KB Video RAM (0x8000-0x9FFF)
+    Cartridge cart; // External rom cartridge
+    // GB-Timer timer;
+    // PPU ppu;
+    // APU apu;
+
+    // RAW MEMORY BANKS
+
+    uint8_t boot_rom[256]; // Internal boot rom (0x0000 - 0x0100);
     uint8_t wram[0x2000]; // 8 KB Work RAM (0xC000-0xDFFF)
-    uint8_t oam[0xA0]; // 160 Bytes Object Attribute Memory (0xFE00-0xFE9F)
     uint8_t hram[0x7F]; // 127 Bytes High RAM (0xFF80-0xFFFE)
 
-    // Eventually link your PPU, APU, and Joypad structs here too:
-    // Interrupt Enable (0xFFFF) and Boot ROM mapping flags can live here
-    uint8_t ie_register;
-    bool boot_rom_mapped;
+    // TO BE PUT IN PPU (here for reference):
+    // uint8_t vram[0x2000]; // 8 KB Video RAM (0x8000-0x9FFF)
+    // uint8_t oam[0xA0]; // 160 Bytes Object Attribute Memory (0xFE00-0xFE9F)
+
+    // NATIVE MMU IO REGISTERS
+
+    bool boot_rom_mapped; // $FF50
+    uint8_t dma_source_high; // $FF46
+    uint8_t ie_register; // $FFFF
 } mmu;
 
 // The core API your CPU (and DMA) will use to interact with the world
