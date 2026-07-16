085d00835ce6c6a5f0c98b0dc06efe1b02bd7ec0
td725 <tom.davies25@imperial.ac.uk>
2026-07-16 20:23:36 +0100

    feat: implimented cpu_init

---
+++ src/cpu.c
@@ -0,0 +1,44 @@
+#include "cpu.h"
+#include <string.h>
+
+void cpu_init(CPU* cpu, mmu* mmu)
+{
+
+    cpu->pc = 0x0000; // Start at the Boot ROM
+    cpu->master_interrupt_enable = false; // Interrupts disabled
+    cpu->halted = false; // CPU starts awake
+    cpu->stopped = false;
+
+    cpu->a = 0x00;
+    cpu->f = 0x00;
+    cpu->b = 0x00;
+    cpu->c = 0x00;
+    cpu->d = 0x00;
+    cpu->e = 0x00;
+    cpu->h = 0x00;
+    cpu->l = 0x00;
+    cpu->sp = 0x0000; // Will be initialized by the Boot ROM
+
+    if (mmu) {
+        cpu->mmu = mmu;
+        mmu->boot_rom_mapped = true;
+        // add initialisation for boot rom (hardcoded)
+
+        memset(mmu->wram, 0, sizeof(mmu->wram));
+        memset(mmu->hram, 0, sizeof(mmu->hram));
+
+        // Interrupt registers start empty
+        mmu->ie_register = 0x00;
+        mmu->if_register = 0xE0; // top 3 bits are always 1
+
+        mmu->dma_active = false;
+        mmu->dma_byte = 0;
+        mmu->dma_delay = 0;
+        mmu->dma_source_high = 0x00;
+        mmu->dma_source_address = 0x0000;
+
+        // external hardware:
+        mmu->cart = NULL; // initially no cartridge inserted
+        // add initalisations for ppu, apu, timer, joypad once implimented
+    }
+}
