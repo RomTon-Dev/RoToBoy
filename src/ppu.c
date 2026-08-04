6ba9c9a6f057ee2fb2750b788e0146e14096bf75
BonBon125 <rongeorge12@icloud.com>
2026-08-04 17:29:18 +0100

    Implemented ppu_read

--- src/ppu.c
+++ src/ppu.c
@@ -31,3 +31,51 @@ void ppu_init(PPU* ppu)
     ppu->request_vblank_interrupt = false;
     ppu->request_stat_interrupt = false;
 }
+
+uint8_t ppu_read(PPU* ppu, uint16_t address)
+{
+    if (address >= 0x8000 && address <= 0x9FFF) {
+        // handle VRAM access
+        if (ppu->current_mode == PPU_MODE_TRANSFER) {
+            // access to VRAM is blocked in this mode
+            return 0xFF;
+        }
+        return ppu->vram[address - 0x8000];
+    } else if (address >= 0xFE00 && address <= 0xFE9F) {
+        if (ppu->current_mode == PPU_MODE_TRANSFER || ppu->current_mode == PPU_MODE_OAM) {
+            // access to OAM is blocked in these modes
+            return 0xFF;
+        }
+        // handle OAM access
+        return ppu->oam[address - 0xFE00];
+    } else if (address >= 0xFF40 && address <= 0xFF4B) {
+        // handle Register access
+        switch (address) {
+        case 0xFF40:
+            return ppu->lcdc;
+        case 0xFF41:
+            return ppu->stat;
+        case 0xFF42:
+            return ppu->scy;
+        case 0xFF43:
+            return ppu->scx;
+        case 0xFF44:
+            return ppu->ly;
+        case 0xFF45:
+            return ppu->lyc;
+        case 0xFF47:
+            return ppu->bgp;
+        case 0xFF48:
+            return ppu->obp0;
+        case 0xFF49:
+            return ppu->obp1;
+        case 0xFF4A:
+            return ppu->wy;
+        case 0xFF4B:
+            return ppu->wx;
+        default:
+            break;
+        }
+    }
+    return 0xFF;
+}
